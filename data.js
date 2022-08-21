f3f516b6d33ae8286f90bc67d4057c5910c88b39
sanjeev <sanjusonyy@gmail.com>
2022-08-21 15:59:30 +0530

    PUSH

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+    {name: 'Sanjeev Kumar', email:'sanjusonyy@gmail.com'},
+    {name: 'MOhit Kumar', email:'sanjusonyy@gmail.com'},
+    {name: 'MOhan Kumar', email:'sanjusonyy@gmail.com'},
+    {name: 'Sohan Kumar', email:'sanjusonyy@gmail.com'}
+]
+module.export=data;
--- nod.js
+++ nod.js
@@ -10,12 +10,39 @@
 // const fs = require('fs').writeFileSync;
 // fs("abc.txt","hello i am sanjeev");
 
-const http = require('http');
+// const http = require('http');
 
-http.createServer((req,resp)=>{
-    resp.write("<h1>hello this is sanjeev</h1>");
-    resp.end();
-    
-}).listen(4500);
+// http.createServer((req,resp)=>{
+//     resp.write("<h1>hello this is sanjeev</h1>");
+//     resp.end();
 
+// }).listen(4500);
 
+// console.log("hello this is sanjeev");
+
+// const http = require('http');
+// const data = require('./data');
+// http.createServer((req, resp) => {
+//     resp.writeHead(201, { 'Content-Type': 'application\json' });
+//     resp.write(JSON.stringify(data));
+//     resp.end();
+// }).listen(6000);
+
+// var http = require('http');
+// var dt = require('./myfirstmodule');
+
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write("The date and time are currently: " + dt.myDateTime());
+//   res.end();
+// }).listen(8080);
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
